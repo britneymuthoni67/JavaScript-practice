77da50ea676dfad8eda03dba8e9e9c91face666f
Britneymuthoni67 <peters.beau8@gmail.com>
2026-03-09 19:16:49 +0300

    Add Variables.js with personal information and logs

---
+++ Variables.js
@@ -0,0 +1,18 @@
+let name = "Britney";
+let age = 19;
+let hobby = "Watching series";
+let isStudent = true;
+
+const country = "Kenya";
+
+let favoritecolor = "Blue";
+let favoritefood = "Rice";
+let dreamjob = "Detective";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+console.log("Country:", country);
+console.log("Favoritecolor:", favoritecolor);
+console.log("Favoritefood:", favoritefood);
+console.log("Dreamjob:", dreamjob);
